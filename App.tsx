8adaa60963ff92110a75eea9eec477a1d421e61b
Yan Wang <wang.yan8@northeastern.edu>
2025-01-13 14:56:04 -0800

    refactor App component to use Input component for user input

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
 import Header from './components/Header';
+import Input from './components/Input';
 
 export default function App() {
-  const [text, setText] = React.useState('');
+
   const appName = 'My First React Native App';
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
-      <TextInput 
-        value={text} 
-        onChangeText={(changeText) => setText(changeText)}
-        placeholder="Type something"
-      />
+      <Input />      
     </View>
   );
 }
