4b3c36c24d79a0d754ed2a7c8fad3acdb3672c65
Yan Wang <wang.yan8@northeastern.edu>
2025-01-13 14:22:46 -0800

    update App component and create Header component, passing props between them

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Header from './components/Header';
 
 export default function App() {
   const appName = 'My First React Native App';
 
   return (
     <View style={styles.container}>
-      <Text>Welcome to {appName}</Text>
       <StatusBar style="auto" />
+      <Header name={appName} />
     </View>
   );
 }
