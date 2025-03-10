8c265c3d5cfa3c0bdf1e1994635a873a77be316c
Yan Wang <wang.yan8@northeastern.edu>
2025-03-10 15:03:19 -0700

    add profile screen and navigation; implement sign out functionality in header

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,4 +1,9 @@
 import { Stack } from "expo-router";
+import PressableButton from "@/components/PressableButton";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {router} from 'expo-router';
+import { signOut } from "firebase/auth";
+import { auth } from "@/Firebase/firebaseSetup";
 
 export default function Layout() {
   return (
@@ -13,12 +18,37 @@ export default function Layout() {
           name="index" 
           options={{
             headerTitle: 'All My Goals', 
+            headerRight: () => (
+              <PressableButton  
+              pressedHandler={() => router.navigate('/(protected)/profile')}
+              pressedStyle={{backgroundColor: 'blue'}}
+              componentStyle={{backgroundColor: 'cornflowerblue', marginRight: 10}}
+            >
+            <Ionicons name="person-circle-outline" size={24} color="black"  />
+           
+            </PressableButton>
+            )
           }}
         />
         <Stack.Screen 
           name="goals/[id]"
           options={{
             title: 'Goal Details', 
+          }}
+        />
+        <Stack.Screen 
+          name="profile"
+          options={{
+            title: 'Profile', 
+            headerRight: () => (
+              <PressableButton  
+                pressedHandler={() => signOut(auth)}
+                pressedStyle={{backgroundColor: 'blue'}}
+                componentStyle={{backgroundColor: 'cornflowerblue', marginRight: 10}}
+              >
+              <Ionicons name="log-out-outline" size={24} color="black"  /> 
+              </PressableButton>
+            )
           }}
         />
       </Stack>
