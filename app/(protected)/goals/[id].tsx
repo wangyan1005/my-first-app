0f793d3a1aeac8332706ab0149ea606478946b2f
Yan Wang <wang.yan8@northeastern.edu>
2025-03-24 13:58:36 -0700

    integrate Firebase storage for image handling in goals

--- app/(protected)/goals/[id].tsx
+++ app/(protected)/goals/[id].tsx
@@ -1,10 +1,13 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router"; 
 import { useEffect, useState} from "react";
 import { goalData, readDocFromDB, updateDB } from "@/Firebase/firestoreHelper";
 import PressableButton from "@/components/PressableButton";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import GoalUsers from "@/components/GoalUsers";
+import { storage } from "@/Firebase/firebaseSetup";
+import { ref } from "firebase/storage";
+import { getDownloadURL } from "firebase/storage";
 
 export default function GoalDetails() {
     const { id } = useLocalSearchParams<{ id: string }>();
@@ -23,6 +26,11 @@ export default function GoalDetails() {
                 if (data !== null) {
                     if (data?.warning) {
                         setWarning(true)
+                    }
+                    if (data.imageUri) {
+                        const imageRef = ref(storage, data.imageUri)
+                        const url = await getDownloadURL(imageRef)
+                        data.imageUri = url
                     }
                     setGoal(data)
                 }
@@ -51,6 +59,9 @@ export default function GoalDetails() {
             <Text style={warning && styles.warning}>You are seeing the 
                 details of the goal with text: {goal?.text} and id: {id}</Text>
             <GoalUsers goalId={id} /> 
+            {goal?.imageUri && (
+                <Image source={{ uri: goal.imageUri }} style={{ width: 100, height: 100 }} />
+            )}
         </View>
     )
 } 
